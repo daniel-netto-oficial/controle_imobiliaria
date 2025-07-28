2dc6bc161cf088cfe2434080d3d0f05e04159103
HenriqueCezana <cezanahenrique@gmail.com>
2025-07-28 10:58:38 -0300

    Criei um formulário que busca imóveis com base no tipo e na cidade onde se localiza, estilizando os elementos adequadamente mantendo o padrão do outro formulário, também fiz a function em javascript que busca esses imóveis e indica caso não tenha nenhum disponível naquelas características, também levando em consideração erros de digitação do usuário e espaços deixados de forma acidental.

--- consultor.js
+++ consultor.js
@@ -58,7 +58,10 @@ function listarImoveis() {
     tbSaida.append(tbody);
 }
 
-// Função para adicionar os dados de um imóvel ao final de todos os vetores e criando uma nova tabela de saída com os novos dados adicionados//
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+// Função para adicionar os dados de um imóvel ao final de todos os vetores, e criando uma nova tabela de saída com os novos dados adicionados//
 const inTipo = document.getElementById("inTipo");
 const inAreaC = document.getElementById("inAreaC");
 const inAreaT = document.getElementById("inAreaT");
@@ -67,7 +70,7 @@ const inBairro = document.getElementById("inBairro");
 const inCidade = document.getElementById("inCidade");
 const formAdicionarImovel = document.getElementById("formAdicionarImovel");
 
-formAdicionarImovel.addEventListener("submit", adicionarImovel)
+formAdicionarImovel.addEventListener("submit", adicionarImovel);
 function adicionarImovel(event) {
     //Evita o recarregamento da página
     event.preventDefault();
@@ -87,9 +90,6 @@ function adicionarImovel(event) {
     vetLogradouro.push(logradouro);
     vetBairro.push(bairro);
     vetCidade.push(cidade);
-
-    //Limpando a tabela
-    tbSaida.innerHTML = "";
 
     //criando thead da tabela
     let thead = document.createElement("thead");
@@ -142,6 +142,97 @@ function adicionarImovel(event) {
     }
     tbSaida.append(tbody);
 
-    // Limpa os campos do formulário após adicionar
+    // Limpa os inputs do formulário após adicionar
     formAdicionarImovel.reset();
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//Função para buscar imóveis conforme o tipo e a cidade , e criando uma nova tabela com os imóveis respectivos//
+const inTipo1 = document.getElementById("inTipo1");
+const inCidade1 = document.getElementById("inCidade1");
+const formBuscaImoveis = document.getElementById("formBuscaImoveis")
+
+formBuscaImoveis.addEventListener("submit", buscarImoveis);
+function buscarImoveis(event) {
+    //Evita o recarregamento da página
+    event.preventDefault();
+    //Limpando a tabela
+    tbSaida.innerHTML = "";
+
+    var tipoPesq = (inTipo1.value).trim().toUpperCase();
+    var cidadePesq = (inCidade1.value).trim().toUpperCase();
+    var tipoAtual = "";
+    var cidadeAtual = "";
+    var imovelCorrespondente = false;
+
+    // Criando o thead da tabela
+    let thead = document.createElement("thead");
+    let trow = document.createElement("tr");
+    let thTipo = document.createElement("th");
+    let thAreaC = document.createElement("th");
+    let thAreaTotal = document.createElement("th");
+    let thLogradouro = document.createElement("th");
+    let thBairro = document.createElement("th");
+    let thCidade = document.createElement("th");
+    thTipo.innerHTML = "Tipo";
+    thAreaC.innerHTML = "Área<br>Construída";
+    thAreaTotal.innerHTML = "Área Total";
+    thLogradouro.innerHTML = "Logradouro";
+    thBairro.innerHTML = "Bairro";
+    thCidade.innerHTML = "Cidade"
+    trow.append(thTipo);
+    trow.append(thAreaC);
+    trow.append(thAreaTotal);
+    trow.append(thLogradouro);
+    trow.append(thBairro);
+    trow.append(thCidade);
+    thead.append(trow);
+    tbSaida.append(thead);
+
+    // Criando o tbody da tabela
+    let tbody = document.createElement("tbody");
+
+    for (let indice = 0; indice < vetTipo.length; indice++) {
+        tipoAtual = vetTipo[indice].toUpperCase();
+        cidadeAtual = vetCidade[indice].toUpperCase();
+
+        if (tipoAtual.includes(tipoPesq) && cidadeAtual.includes(cidadePesq)) {
+            imovelCorrespondente = true;
+
+            // Criando o restante da tabela com os imóveis com os tipos e a cidade digitada //
+            let trow = document.createElement("tr");
+            let tdTipo = document.createElement("td");
+            let tdAreaC = document.createElement("td");
+            let tdAreaTotal = document.createElement("td");
+            let tdLogradouro = document.createElement("td");
+            let tdBairro = document.createElement("td");
+            let tdCidade = document.createElement("td");
+            tdTipo.innerHTML = vetTipo[indice] || "Desconhecido";
+            tdAreaC.innerHTML = vetAreaC[indice] ? vetAreaC[indice] + " m²" : "Não calculado";
+            tdAreaTotal.innerHTML = vetAreaT[indice] ? vetAreaT[indice] + " m²" : "Não calculado";
+            tdLogradouro.innerHTML = vetLogradouro[indice] || "Desconhecido";
+            tdBairro.innerHTML = vetBairro[indice] || "Desconhecido";
+            tdCidade.innerHTML = vetCidade[indice] || "Desconhecido";
+            trow.append(tdTipo);
+            trow.append(tdAreaC);
+            trow.append(tdAreaTotal);
+            trow.append(tdLogradouro);
+            trow.append(tdBairro);
+            trow.append(tdCidade);
+            tbody.append(trow);
+        }
+    }
+
+    // Mostra uma mensagem dizendo que nenhum imóvel foi encontrado
+    if(imovelCorrespondente == true){
+        tbSaida.append(tbody);
+    } else {
+        tbSaida.innerHTML = "";
+        let h2 = document.createElement("h2");
+        h2.innerHTML = "Nenhum imóvel correspondente foi encontrado.";
+        tbSaida.appendChild(h2);
+    }
+    // Limpa os inputs do formulário após o click no botão
+    formBuscaImoveis.reset();
 }
